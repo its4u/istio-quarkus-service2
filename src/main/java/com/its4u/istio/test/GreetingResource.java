e61ce628db4ffc34f7abf873cdc0214bd669e12f
Arnaud Lalouette <arnaud.lalouette@its4u.com>
2021-02-04 17:37:52 +0100

    Initial version

---
+++ src/main/java/com/its4u/istio/test/GreetingResource.java
@@ -0,0 +1,19 @@
+package com.its4u.istio.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/hello")
+@RegisterRestClient
+public interface GreetingResource {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	String hello(@QueryParam("name") String name);
+
+}
